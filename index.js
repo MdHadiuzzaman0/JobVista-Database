885a1657e0dcc4803bc33d692863fb0a1dafa8a6
Mdhadiuzzaman0 <mdhadiuzzaman483@gmail.com>
2026-05-30 00:09:34 +0600

    feat: set up express server for JobVista with mongodb connection

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.MONGODB_URL
+const port = process.env.PORT
+
+app.use(cors());
+app.use(express.json())
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+async function run() {
+  try {
+    await client.connect();
+
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
